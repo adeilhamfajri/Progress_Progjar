8682fca57b941e69710422c0b9ed27c5da51042f
bils <nabillasabbaha@gmail.com>
2015-12-23 13:24:33 +0700

    Bagian Server

---
+++ Server_Game/src/server_game/Server_Game.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server_game;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Ade Ilham Fajri
+ */
+
+class ClientThread extends Thread{
+    private DataInputStream is = null;
+    private PrintStream os = null;
+    private Socket client_socket = null;
+    private final ClientThread[] threads; //"threads" bukan "thread"
+    private int max_clients;
+    
+    public ClientThread(Socket client_socket, ClientThread[] threads){
+        this.client_socket = client_socket;
+        this.threads = threads;
+        max_clients = threads.length;
+    }
+    
+    @Override
+    public void run(){
+        int max_clients = this.max_clients;
+        ClientThread[] threads = this.threads;
+        
+        
+        try{
+            //input output streams
+            is = new DataInputStream(client_socket.getInputStream());
+            os = new PrintStream(client_socket.getOutputStream());
+            String input;
+            String [] splitinput;
+            input = is.readLine();
+            
+            os.println("Bismillah...");
+            
+            
+        }catch(Exception e) {
+            //.....
+        }
+    }
+}
+
+public class Server_Game {
+
+    //Instansiasi ServerSocket dan Socket
+    private static ServerSocket server_socket = null;
+    private static Socket client_socket = null;
+    
+    private static final int max_clients = 2;
+//    private static final clientThread[] thread = new 
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       
+    }
+}
